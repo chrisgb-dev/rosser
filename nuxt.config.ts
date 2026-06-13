d30c37e7ee6f774dff0a0f4438a92d64dfcb50a2
Chris Gardiner-Bill <cbill.uk@gmail.com>
2026-06-13 22:08:16 +1000

    omitting OgImage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,17 +43,8 @@ export default defineNuxtConfig({
       stripePublishableKey: '',
     },
   },
-  seo: {
-    ogImage: false
-  },
   devtools: { enabled: true },
-  modules: [
-  '@nuxt/content', 
-  "@nuxtjs/seo",
-  'nuxt-umami',
-  '@nuxt/ui',
-  '@nuxt/image',
-],
+  modules: ['@nuxt/content', 'nuxt-umami', '@nuxt/ui', '@nuxt/image', '@nuxtjs/robots', '@nuxtjs/sitemap', 'nuxt-schema-org', 'nuxt-link-checker', 'nuxt-seo-utils'],
   umami: {
     id: "5a9ba10a-7814-41e2-a7b8-538b024dec5b",
     host: "https://cloud.umami.is",
@@ -70,5 +61,12 @@ export default defineNuxtConfig({
     '/posts/2018/08/11/review-omnioutliner/': { redirect: 'https://chrisrosser.medium.com/omnioutliner-review-68bd3b1e5735' },
     '/posts/2021/04/27/how-to-create-a-parchment-background-in-procreate/': { redirect: 'https://chrisrosser.medium.com/how-to-create-a-parchment-background-in-procreate-f1517b129215' },
     '/posts/2020/04/15/how-to-create-a-paperback-with-apple-pages/': { redirect: 'https://chrisrosser.substack.com/p/how-to-create-a-paperback-with-apple' }
-  }
+  },
+  optimizeDeps: {
+      include: [
+        '@unhead/schema-org/vue',
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
 })
